9e39fb5a05097eb74e626a72b0ba009a9bb6fe72
tamir976 <48364838+tamir976@users.noreply.github.com>
2024-01-24 00:23:16 +0100

    developed table from json file

--- BAS_System/src/app/app.routes.ts
+++ BAS_System/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent,
+    },
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "employee-list",
+        component: EmployeeComponent
+    },
+    {
+        path: "register-employee",
+        component: RegisterComponent
+    }
+];
---
+++ BAS_System/src/app/employee/interfaces/employee.ts
@@ -0,0 +1,9 @@
+export interface IEmployee{
+    id: number,
+    firstName: string,
+    lastName: string,
+    email: string,
+    arrivedTime: Date,
+    delayedTime: Date,
+    year: number
+}
